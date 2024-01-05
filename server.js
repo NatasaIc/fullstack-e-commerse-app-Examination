7667b9296d6fafceb1b852ba3daba39196e41e96
NatasaIc <iconomou.ng@gmail.com>
2024-01-05 18:27:56 +0100

    adding middleware morgan and users data

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const products = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/products.json`)
@@ -12,9 +26,10 @@ const products = JSON.parse(
 // ROUTE HANDLERS
 
 const getAllProducts = (req, res) => {
-  console.log();
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: products.lenght,
     data: {
       products: products,
