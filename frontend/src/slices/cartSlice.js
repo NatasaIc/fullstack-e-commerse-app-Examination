47422ed19efd0c035803938d659a446d8c9fc247
NatasaIc <iconomou.ng@gmail.com>
2024-01-21 09:00:35 +0100

    cart function: add to cart, Qty and cart utils file

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,13 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { udateCart } from '../utils/cartUtils';
 
+// Retrieve the cart data from local storage or initialize an empty cart
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
 
+// Create a slice of the Redux store for the cart
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  // Define reducers for actions that can modify the state
+  reducers: {
+    addToCart: (state, action) => {
+      // Extract the item payload from the action
+      const item = action.payload;
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      // If the item exists, update its quantity; otherwise, add it to the cart
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x,
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      return udateCart(state);
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,26 @@
+export const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+export const udateCart = (state) => {
+  // Calculate items price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
+  );
+
+  // Calculate shipping price (If order is over $50 then free, else $6 shipping)
+  state.shippingPrice = addDecimals(state.itemsPrice > 50 ? 0 : 10);
+
+  // Calculate tax price (15% tax)
+  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+  // Calculate total price
+  state.totalPrice = addDecimals(
+    Number(state.itemsPrice) +
+      Number(state.shippingPrice) +
+      Number(state.taxPrice),
+  );
+  localStorage.setItem('cart', JSON.stringify(state));
+
+  return state;
+};
