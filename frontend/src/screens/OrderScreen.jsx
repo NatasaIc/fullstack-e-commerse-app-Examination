9849b90c27cc09c10cd768598675def09bfd250d
NatasaIc <iconomou.ng@gmail.com>
2024-01-30 11:54:55 +0100

    working on orderScreen

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,65 @@
+import { Link, useParams } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice';
+
+const OrderScreen = () => {
+  const { id: orderId } = useParams();
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    error,
+  } = useGetOrderDetailsQuery(orderId);
+
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger" />
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Name: </strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong> {order.user.email}
+              </p>
+              <p>
+                <strong>Address: </strong>
+                {order.shippingAddress.adress}, {order.shippingAddress.city}{' '}
+                {order.shippingAddress.postalCode},{' '}
+                {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered on {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item></ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>Column</Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderScreen;
