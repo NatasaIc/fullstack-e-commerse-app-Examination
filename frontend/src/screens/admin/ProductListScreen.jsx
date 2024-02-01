03b2137b2ed4292c62dead29d2dcb65d3a1a6ce8
NatasaIc <iconomou.ng@gmail.com>
2024-02-01 13:15:08 +0100

    same problem with map products in ProductListScreen. Issue fixed with array.isArray ?

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -8,7 +8,65 @@ import { useGetProductsQuery } from '../../slices/productsApiSlice';
 const ProductListScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
-  return <div>ProductListScreen</div>;
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="btn-sm m3">
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(products) ? (
+                products.map((product) => (
+                  <tr key={product._id}>
+                    <td>{product._id}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.category}</td>
+                    <td>{product.brand}</td>
+                    <td>
+                      <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                        <Button variant="light" className="btn-sm mx-2">
+                          <FaEdit />
+                        </Button>
+                      </LinkContainer>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="6">No products available</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProductListScreen;
