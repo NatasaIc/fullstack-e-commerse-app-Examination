3ce07e77d8d4c8ed2df0844ce65edf10b1f89276
NatasaIc <iconomou.ng@gmail.com>
2024-02-05 07:18:58 +0100

    working on userListScreen list users, delete and edit

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -1,17 +1,31 @@
+import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
 import { FaTimes, FaTrash, FaEdit, FaCheck } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-import { useGetUsersQuery } from '../../slices/usersApiSlice';
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+} from '../../slices/usersApiSlice';
 import { toast } from 'react-toastify';
 
 const UserListScreen = () => {
   const { data: users, isLoading, error, refetch } = useGetUsersQuery();
-  console.log(users);
+
+  const [deleteUser] = useDeleteUserMutation();
 
   const handleDelete = async (id) => {
-    console.log(users);
+    if (window.confirm('Are you sure?')) {
+      try {
+        await deleteUser(id);
+        toast.success('User deleted');
+        refetch();
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+        console.log(err);
+      }
+    }
   };
 
   return (
@@ -35,38 +49,37 @@ const UserListScreen = () => {
               </tr>
             </thead>
             <tbody>
-              {Array.isArray(users) &&
-                users.map((user) => (
-                  <tr key={user._id}>
-                    <td>{user._id}</td>
-                    <td>{user.photo}</td>
-                    <td>{user.name}</td>
-                    <td>
-                      <a href={`mailto:${user.email}`}>{user.email}</a>
-                    </td>
-                    <td>
-                      {user.isAdmin ? (
-                        <FaCheck style={{ color: 'green' }} />
-                      ) : (
-                        <FaTimes style={{ color: 'red' }} />
-                      )}
-                    </td>
-                    <td>
-                      <LinkContainer to={`/admin/user/${user._id}/edit`}>
-                        <Button variant="light" className="brn-sm">
-                          <FaEdit />
-                        </Button>
-                      </LinkContainer>
-                      <Button
-                        variant="danger"
-                        className="btn-sm"
-                        onClick={() => handleDelete(user._id)}
-                      >
-                        <FaTrash style={{ color: 'white' }} />
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.photo}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <a href={`mailto:${user.email}`}>{user.email}</a>
+                  </td>
+                  <td>
+                    {user.isAdmin ? (
+                      <FaCheck style={{ color: 'green' }} />
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                      <Button variant="light" className="brn-sm">
+                        <FaEdit />
                       </Button>
-                    </td>
-                  </tr>
-                ))}
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => handleDelete(user._id)}
+                    >
+                      <FaTrash style={{ color: 'white' }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </Table>
         </>
